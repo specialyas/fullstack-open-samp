a8a3e47d12bf0f676833f0e800d8db0e340e0c20
specialyas <holla2specialyusuf@gmail.com>
2022-06-14 13:36:02 +0100

    refactor post

--- index.js
+++ index.js
@@ -48,12 +48,32 @@ app.delete('/api/notes/:id', (request, response) => {
     response.status(204).end()
 })
 
-app.post('/api/notes', (request, response) => {
-    const maxId = notes.length > 0
+const generateId = () => {
+     const maxId = notes.length > 0
     ? Math.max(...notes.map(n => n.id))
     : 0
-    const note = request.body
-    console.log(note)
+
+    return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+   const body = request.body
+
+   if (!body.content) {
+       return response.status(400).json({
+           error: 'content missing'
+       })
+   }
+
+   const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+   }
+
+    notes = notes.concat(note)
+
     response.json(note)
 })
 
